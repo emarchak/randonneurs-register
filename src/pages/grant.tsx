b8dd802303a87120b04fea0456673ebb8418edbb
Erin Marchak <erin@emarchak.com>
2023-03-23 20:39:53 +0000

    Updates for sponsorship.

--- src/pages/grant.tsx
+++ src/pages/grant.tsx
@@ -8,6 +8,8 @@ import { Layout } from "src/components/layout"
 import { Link } from "src/components/Link"
 import { SeasonsCta } from "src/components/seasons"
 import { SEO } from "src/components/seo"
+import {BlackArrow} from "src/components/grant/BlackArrow"
+import {Urbane} from "src/components/grant/Urbane"
 
 const GrantPage = () => (
   <Layout>
@@ -18,7 +20,7 @@ const GrantPage = () => (
       <ContentChild>
         <p>The Randonneurs Ontario Access Grant is designed to reduce barriers to the ultra-distance riding community. This access grant is for someone from a group under-represented in the ultra-distance riding community that would have otherwise not been able to participate in Randonneurs Ontario events.</p>
         <p>We have been heavily inspired by the work of <Link href="https://radicaladventureriders.com/scholarship">RAR’s SJ Brooks Scholarship</Link>.</p>
-        <LinkButton primary block href="FORMURL">Apply for the RO Access Grant</LinkButton>
+        <LinkButton primary block href="https://forms.gle/2zWxxf9Pix3gQQM7A">Apply for the RO Access Grant</LinkButton>
       </ContentChild>
       <ContentChild>IMG</ContentChild>
     </ContentWrapper>
@@ -43,16 +45,16 @@ const GrantPage = () => (
       <h3>Grant</h3>
       <p>This grant covers the registration fees and equipment requirements for Randonneurs Ontario events, for a total value of $2000. This includes:
       <ul>
-        <li>Randonneurs Ontario membership fees for 2023</li>
-          <li>$1500 worth of support from one of our bicycle shops sponsors for:
+        <li><strong>Randonneurs Ontario membership fees for 2023</strong></li>
+          <li><strong>$1500 worth of support from one of our bicycle shops sponsors, such as:</strong>
             <ul>
-              <li>Bicycle tune-up, repair, and/or misc equipment.</li>
+              <li>Bicycle tune-up, repair, and/or misc equipment</li>
               <li>Required night riding equipment, such as sufficient lighting and a reflective vest or sash</li>
               <li>Approved cycling helmet</li>
               <li>Equipment bags sufficient for a 200km+ ride</li>
               </ul>
         </li>
-        <li>$500 worth of support for use during a Randonneurs Ontario event for<ul>
+        <li><strong>$500 worth of support for use during a Randonneurs Ontario event, such as</strong><ul>
           <li>Meals in-between the controls</li>
           <li>Lodging for overnight controls, if needed</li>
           <li>Extra equipment required to complete a brevet, such as flat repair kits</li>
@@ -60,7 +62,7 @@ const GrantPage = () => (
             </ul>
         </li>
         </ul></p>
-      <p>One recipient will also receive a bicycle bag suitable for air travel, donated by Internet Light and Power.</p>
+      <p><strong>One recipient will also receive a bicycle bag suitable for air travel, donated by Internet Light and Power.</strong></p>
 
       <p>This grant does not cover the Ontario Cycling Association membership. OCA Membership is included in all racing licenses, or it can be obtained with an OCA Affiliate Club Membership.</p>
       </Callout>
@@ -77,21 +79,23 @@ const GrantPage = () => (
     </ContentWrapper>
     <ContentWrapper>
       <h3>Supporters</h3>
-      This grant is possible thanks to the generosity of:
+      If your company would like to get involved, please email us at grant@randonneurs.to. This grant is possible thanks to the generosity of:
     </ContentWrapper>
     <ContentWrapper container>
       <ContentChild><center><Link href="http://www.mtc.gov.on.ca/en/home.shtml">Ontario Ministry of Heritage,<br/> Sport, Tourism & Culture Industries </Link></center></ContentChild>
       <ContentChild><center><Link href="https://ontariocycling.org">Ontario Cycling<br />through their Member Club Rebuild program</Link></center></ContentChild>
     </ContentWrapper>
     <ContentWrapper container>
-      <ContentChild><center><Link href="https://www.blackarrowcycles.ca">Black Arrow Cycles</Link></center></ContentChild>
+      <ContentChild><center>
+        <Link href="https://www.blackarrowcycles.ca"><BlackArrow />Black Arrow Cycles</Link>
+      </center></ContentChild>
       <ContentChild><center><Link href="https://www.ilap.com">Internet Light and Power</Link></center></ContentChild>
-      <ContentChild><center><Link href="https://www.ucycle.com">Urbane Cyclist</Link></center></ContentChild>
+      <ContentChild><center><Link href="https://www.ucycle.com"><Urbane/> Urbane Cyclist</Link></center></ContentChild>
     </ContentWrapper>
     <SeasonsCta />
     <LatestsPosts />
   </Layout>
-)
+)}
 
 export const Head = () => <SEO
   title="Randonneurs Ontario Access Grant"
