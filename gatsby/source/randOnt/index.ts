fc554214c9d7f6232499bca24335bd38da595be4
Erin Marchak <erin@emarchak.com>
2023-12-28 21:11:55 +0000

    remove server side redirect.

--- gatsby/source/randOnt/index.ts
+++ gatsby/source/randOnt/index.ts
@@ -2,5 +2,4 @@ export const nodeType = 'Event'
 
 export { createSchemaCustomization as createEventSchemaCustomization } from './createSchemaCustomization'
 export { sourceNodes as sourceEventNodes } from './sourceNodes'
-export { onCreateNode as onCreateEventNode } from './onCreateNode'
 export { createPages as createEventPages } from './createPages'
