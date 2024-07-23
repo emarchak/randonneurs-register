048469428d6d7a03821238509f3e041939d93d73
Erin Marchak <erin@emarchak.com>
2024-07-22 20:07:23 -0400

    feat(perm): update content on perm registration page

--- src/pages/registration/permanent.tsx
+++ src/pages/registration/permanent.tsx
@@ -28,19 +28,22 @@ export const PermanentRegistration = ({ path }: PageProps) => {
         </p>
 
         <Callout alternative>
-          <p>
-            <strong>A Permanent ride</strong> is one of the existing Randonneurs
-            Ontario brevet routes ridden outside of the normal Brevet schedule.
-            Any current Randonneurs Ontario member may propose riding a
-            Permanent, with at least 2 weeks notice before the proposed date.
-            Once approved, there can be no changes to the request, except at the
-            discretion of the Chapter VP and Treasurer.
+          <p><strong>A Permanent ride</strong> is one of the existing Randonneurs Ontario brevet routes ridden outside of the normal Brevet schedule. Any current Randonneurs Ontario member may propose riding a Permanent. They can be run as individual rides, or they can be organized with several club members choosing to ride the same course on the same day.</p>
+
+          <p>Permanents are not sanctioned by ACP, and therefore do not take the place of brevets for the ACP Super Randonneur award, nor will they count for qualification for PBP. However they remain a great way to engage in the sport of randonneuring at the club level. 
           </p>
-          <p>
-            Standard brevet rules apply for Permanents. Control cards must be
-            completed during the ride, including start and end. Permanents do
-            not take the place of brevets for the ACP Super Randonneur award,
-            nor will they count for qualification for PBP.
+          <hr />
+          <p>Rules governing permanents are as follows: </p>
+
+          <p><strong>Registration: </strong>
+          <ul><li>Members in good standing may schedule such a ride as late as 20:00 the day before the intended ride date. We ask that new members provide a 48 hour grace period to ensure their membership is validated by the route's chapter VP prior to the ride. </li>
+          <li>Once approved, changes to the request must be communicated with, and approved by, the route's regional Chapter VP. </li><ul>
+          </p>
+
+          <p><strong>Riding a Permanent: </strong>
+          <ul><li>Standard brevet rules apply for Permanents. Control cards must be completed during the ride, including start and end. Riders are responsible for supplying their own control cards.</li>
+          <li>Riders may start the route at any point along the route (usually a control). They may also choose to ride the route in reverse. It is the rider's responsibility to supply their own navigation (cue sheet, GPS track) in these exceptional cases.</li>
+          <li>As with brevets, once the ride is completed, proof of passage must be sent to the route's regional chapter VP for club sanctioning. This is typically done in the form of control card photos (front and back), as well as a GPX file, or link to an online record of the travel log. </li></ul>
           </p>
           <p>
             <Link href="http://randonneursontario.ca/Permanents/Permanents.html">
